a21e289a69edbcef76e75014d9a96a6b82fec591
Francesco Valentini <francesco.valentini930@gmail.com>
2026-02-05 21:20:47 +0100

    Better JSON parser

--- template.go
+++ template.go
@@ -12,17 +12,12 @@ func renderTemplate(tmplContent []byte, jsonData []byte, logger *AppLogger) (str
 	logger.Info("Parsing JSON data")
 
 	// Parse JSON into a generic map
-	var data map[string]interface{}
+	var data interface{}
 	if err := json.Unmarshal(jsonData, &data); err != nil {
 		return "", fmt.Errorf("failed to parse JSON: %w", err)
 	}
 
-	logger.Info("JSON parsed successfully (%d top-level keys)", len(data))
-	if logger.verbose {
-		for key := range data {
-			logger.Debug("  - JSON key: %s", key)
-		}
-	}
+	showDataType(data, logger)
 
 	logger.Info("Parsing template")
 
@@ -44,4 +39,28 @@ func renderTemplate(tmplContent []byte, jsonData []byte, logger *AppLogger) (str
 	logger.Info("Template executed successfully (output size: %d bytes)", len(result))
 
 	return result, nil
+}
+
+func showDataType(data interface{}, logger *AppLogger) {
+	switch v := data.(type) {
+
+	case map[string]interface{}:
+		logger.Info("JSON parsed successfully (%d top-level keys)", len(v))
+		if logger.verbose {
+			for key := range v {
+				logger.Debug("  - JSON key: %s", key)
+			}
+		}
+
+	case []interface{}:
+		logger.Info("JSON parsed successfully (root array, %d elements)", len(v))
+		if logger.verbose {
+			for i, el := range v {
+				logger.Debug("  - element #%d type: %T", i, el)
+			}
+		}
+
+	default:
+		logger.Warn("JSON parsed, root type is %T", v)
+	}
 }
